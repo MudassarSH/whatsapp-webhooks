0849bd0b66ab2ed65e7595737aa1f9494ab1decd
Mudassar <mudassar.h1200@gmail.com>
2026-02-15 17:45:34 +0500

    first

---
+++ src/components/privacy-policy.ts
@@ -0,0 +1,11 @@
+export const PRIVACY_POLICIES = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy</title>
+</head>
+<body>
+    <h2>This is the privacy policy page</h2>
+</body>
+</html>`
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import { PRIVACY_POLICIES } from "./components/privacy-policy";
+dotenv.config();
+const app = express()
+app.use(express.json())
+const port = process.env.PORT
+const token = process.env.WEBHOOK_VERIFY_TOKEN
+
+app.get('/webhooks', (req, res) => {
+  const mode = req.query["hub.mode"]
+  const verifyToken = req.query["hub.verify_token"]
+  const challenge = req.query["hub.challenge"]
+  if (mode === "subscribe" && verifyToken === token) {
+    res.status(200).send(challenge)
+  } else {
+    res.sendStatus(403)
+  }
+})
+
+app.post("/webhooks", (req, res) => {
+  res.sendStatus(200)
+  console.log("Webhook event:", JSON.stringify(req.body, null, 2));
+  const value = req.body.entry?.[0]?.changes?.[0]?.value;
+
+  if (!value) return;
+
+  // Incoming message
+  if (value.messages) {
+    console.log("Incoming message:", value.messages);
+  }
+
+  // Delivery status update
+  if (value.statuses) {
+    console.log("Message status update:", value.statuses);
+  }
+});
+
+app.get("/privacy-policy", (req, res) => {
+  res.send(PRIVACY_POLICIES)
+})
+app.get("/", (req, res) => {
+  res.send("Hello world!")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
